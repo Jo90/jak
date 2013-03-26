057a6ca6cc1a964bebcde342e37fd337144d3b0e
Amelita <amelita@synergifirst.com.au>
2013-03-26 15:05:58 +1100

    db/prop/common.php

--- db/prop/common.php
+++ db/prop/common.php
@@ -4,6 +4,72 @@
  */
 namespace jak;
 
+function prop_getAnswerMatrix($criteria) {
+    global $mysqli;
+    $r = new \stdClass;
+    $r->criteria = $criteria;
+    $cnd   = '';
+    $limit = '';
+
+    if (isset($criteria->answerMatrixIds) && is_array($criteria->answerMatrixIds) && count($criteria->answerMatrixIds) > 0) {
+        $answerMatrixIds = implode(',', $criteria->answerMatrixIds);
+        $cnd = "where id in ($answerMatrixIds)";
+    }
+
+	if (isset($criteria->propPartIds) && is_array($criteria->propPartIds) && count($criteria->propPartIds) > 0){
+        $propPartIds = implode(',', $criteria->propPartIds);
+        $cnd = "where propPart in ($propPartIds)";
+	}
+	
+    if (isset($criteria->rowLimit)) {
+        $limit = ' limit ' . $criteria->rowLimit;
+    }
+
+    if ($stmt = $mysqli->prepare(
+        "select *
+           from `answerMatrix` $cnd $limit"
+    )) {
+        $r->success = $stmt->execute();
+        $r->rows = $mysqli->affected_rows;
+        $r->data = \jak\fetch_result($stmt,'id');
+        $stmt->close();
+    }
+    return $r;
+}
+
+function prop_getPropPart($criteria) {
+    global $mysqli;
+    $r = new \stdClass;
+    $r->criteria = $criteria;
+    $cnd   = '';
+    $limit = '';
+
+    if (isset($criteria->propPartIds) && is_array($criteria->propPartIds) && count($criteria->propPartIds) > 0) {
+        $propPartIds = implode(',', $criteria->propPartIds);
+        $cnd = "where id in ($propPartIds)";
+    }
+
+	if (isset($criteria->jobIds) && is_array($criteria->jobIds) && count($criteria->jobIds) > 0){
+        $jobIds = implode(',', $criteria->jobIds);
+        $cnd = "where job in ($jobIds)";
+	}
+	
+    if (isset($criteria->rowLimit)) {
+        $limit = ' limit ' . $criteria->rowLimit;
+    }
+
+    if ($stmt = $mysqli->prepare(
+        "select *
+           from `propPart` $cnd $limit"
+    )) {
+        $r->success = $stmt->execute();
+        $r->rows = $mysqli->affected_rows;
+        $r->data = \jak\fetch_result($stmt,'id');
+        $stmt->close();
+    }
+    return $r;
+}
+
 function prop_getPropPartType($criteria) {
     global $mysqli;
     $r = new \stdClass;

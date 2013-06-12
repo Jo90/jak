73273526faa2933d7727ed576b2a0d9da1f0bed8
Joseph Douglas <joe@dargaville.net>
2013-06-12 12:57:04 +1200

    error 404 cleanup

--- error404.php
+++ error404.php
@@ -1,6 +1,4 @@
-<?php /* error404.php
-
-        JAK
+<?php /* //error404.php
 */
 ?>
 <html>
@@ -9,18 +7,17 @@
     <div id="content">
         <h1>JAK</h1>
         <h2>Error - Page not found</h2>
-        <br/>
         <h4>Sorry! - You have reached a page that has changed or no longer exists.</h4>
+        <a href="/">Our Home Page</a>
         <br/>
         <h4>Please check the following<h4>
-        <br/>
         <ul>
             <li>Please update your bookmarks!</li>
             <li>Please notify us of no longer valid links.</li>
             <li>Check the URL entered.</li>
             <li>Can you connect to other pages?.</li>
         <ul>
-     <br/>
+    </div>
 </div>
 </body>
 </html>
